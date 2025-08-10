2d5dda843857f591c20262ead8b9f73debf7ec47
Briza5 <BrizaL555@gmail.com>
2025-08-10 10:19:00 +0200

    Add invoice date

--- main.py
+++ main.py
@@ -7,10 +7,20 @@ filepaths = glob.glob("invoices/*.xlsx") # glob to find all .xlsx files in the i
 
 for filepath in filepaths: # Iterate over each file path in the list
     df = pd.read_excel(filepath, sheet_name="Sheet 1") # Read the Excel file into a DataFrame and specify the sheet name
+    
     pdf = FPDF(orientation="P", unit="mm", format="A4") # Create a new PDF object
     pdf.add_page() # Add a new page to the PDF
+    
     filename = Path(filepath).stem # Extract the file name without the extension
-    invoice_nr = filename.split("-")[0] # Split the file name to get the invoice number from the first part of the name
+    invoice_nr, date = filename.split("-") # Split the filename into invoice number and date based on the hyphen
+
     pdf.set_font("Times", size=16, style="B") # Set the font for the PDF
-    pdf.cell(w=58,h=8, txt=f"Invoice nr. {invoice_nr}") # Add a cell with the invoice number
+    pdf.cell(w=58,h=8, txt=f"Invoice nr. {invoice_nr}", ln=1) # Add a cell with the invoice number and move to the next line
+   
+    pdf.set_font("Times", size=16, style="B") # Set the font for the PDF
+    pdf.cell(w=58,h=8, txt=f"Date: {date}") # Add a cell with the date
+
+   
+   
+   
     pdf.output(f"pdfs/{filename}.pdf") # Save the PDF to the specified directory with the same name as the Excel file
